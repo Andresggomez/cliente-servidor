5b314365f10f9f3e12f987077a2edc7ec453632c
Dexter <LefaucetCapital@gmail.com>
2022-09-01 21:34:09 -0500

    Se crea archivo git ignore y ademas se incluye una funcion hash para obtener informacion sobre md5 de cada archivo subido por el usuario

--- send-files/clientes/cliente1/hash.py
+++ send-files/clientes/cliente1/hash.py
@@ -5,5 +5,5 @@ md5_hash = hashlib.md5()
 with open(filename, "rb") as f:
     for byte_block in iter(lambda: f.read(4096), b""):
         md5_hash.update(byte_block)
-    print(md5_hash.hexdigest())
+    print(md5_hash.hexdigest() )
 
